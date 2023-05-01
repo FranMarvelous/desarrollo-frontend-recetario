b5e4a43227a24a43ef61fae6e8e499f41f1ce6b4
FranMarvelous <franmarvelous@gmail,com>
2023-04-30 22:12:45 -0400

    reorganización de archivos en sus respectivas carpetas dentro de assets

---
+++ assets/js/prueba.js
@@ -0,0 +1,28 @@
+const formulario = document.querySelector("form");
+formulario.addEventListener('submit', (evento) => {
+    //CANCELO PROPAGACION DE EVENTO SUBMIT:
+    evento.preventDefault();
+    //RECUPERAR DATOS DEL FORMULARIO
+    const email = document.querySelector("#email").value;
+    const contrasena = document.querySelector("#contrasena").value;
+    
+    //CONFIGURAMOS OBJETO A ENVIAR A WEB SERVICE X POST
+    const usuario = {
+        email, //email: email
+        contrasena //contrasena: contrasena
+    };
+ 
+    //LLAMADA REMOTA
+    fetch('https://moored-psychedelic-parmesan.glitch.me/registro-usuario', {
+            method: 'POST',
+            body: JSON.stringify(usuario),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response=> response.json())
+        .then(data => console.dir(data))
+        .catch(error => console.error(error))
+    ; 
+} );
+
