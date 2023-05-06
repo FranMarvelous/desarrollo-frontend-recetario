d500e19da57f538ed7b774125ed47e4082b172a7
FranMarvelous <franmarvelous@gmail,com>
2023-05-06 18:47:02 -0400

    creacion de carpeta admin en main branch

--- admin-src/next.config.js
+++ admin-src/next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  /*
   output: 'export',
+  //esta será la ruta con la que se reescribirán los enlaces
   basePath: '/admin',
   trailingSlash: true,
   images: {
     unoptimized: true
-  } */
+  } 
 }
 
 module.exports = nextConfig
